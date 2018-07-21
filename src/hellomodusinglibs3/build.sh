0be7d003aeedbbb04d9871505e18911614bc480f
John Bannick <jbannick@7128.com>
2018-07-21 12:44:26 -0400

    Update build.sh

--- src/hellomodusinglibs3/build.sh
+++ src/hellomodusinglibs3/build.sh
@@ -6,9 +6,9 @@ mkdir -p out
 cp $EBUS_HOME/eventbus-1.4.jar .
 
 javac --module-path . -d out --module-source-path src -m appmod\
-    src/appmod/kata/app/HelloModUsingLib3.java \
+    src/appmod/kata/app/HelloModUsingLibs3.java \
     src/appmod/kata/app/Publisher.java \
     src/appmod/kata/app/Subscriber.java \
     src/appmod/module-info.java
 
-jar -cfe appmod.jar kata.app.HelloModUsingLib3 -C out/appmod .
+jar -cfe appmod.jar kata.app.HelloModUsingLibs3 -C out/appmod .
